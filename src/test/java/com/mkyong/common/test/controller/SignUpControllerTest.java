6329945b884f95cd363b6830ee066d2d36e9fde8
Firoz Fazil <firoz.fazil@gmail.com>
2012-09-30 16:37:30 +0530

    Initial import.

---
+++ src/test/java/com/mkyong/common/test/controller/SignUpControllerTest.java
@@ -0,0 +1,71 @@
+package com.mkyong.common.test.controller;
+
+import com.mvc.test.model.Customer;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class})
+@ContextConfiguration(locations = {
+        "classpath:mvc-dispatcher-servlet.xml"
+})
+public class SignUpControllerTest {
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+
+
+    @Autowired
+    private RequestMappingHandlerAdapter handlerAdapter;
+
+    @Autowired
+    private RequestMappingHandlerMapping handlerMapping;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        this.request = new MockHttpServletRequest();
+        request.setContentType("application/json");
+        this.response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void testSignUp(){
+        request.setMethod("POST");
+        request.setRequestURI("/customer/signup");
+
+        final ModelAndView mav;
+        Object handler;
+        try
+        {
+            Customer customer = new Customer();
+            customer.setAge(11);
+            customer.setName("Test");
+            request.setParameter("name", "Test");
+            request.setParameter("age","11");
+            handler = handlerMapping.getHandler(request).getHandler();
+            mav = handlerAdapter.handle(request, response, handler);
+            Assert.assertEquals(200, response.getStatus());
+        }
+        catch (Exception e){
+             e.printStackTrace();
+        }
+    }
+
+}
